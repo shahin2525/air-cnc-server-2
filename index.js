0433e9e8a009bf2327f889891deffdbe10698ef7
shahin2525 <shahin451999@gmail.com>
2023-10-09 22:34:28 -0700

    my-listing-route added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -30,7 +30,7 @@ async function run() {
     const roomsCollection = client.db("airCncDB2").collection("rooms");
     const bookingsCollection = client.db("airCncDB2").collection("bookings");
 
-    // put route
+    // put users
 
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
@@ -43,22 +43,88 @@ async function run() {
       const result = await usersCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
-
-    // // post room
-    // app.post("/rooms", async (req, res) => {
-    //   const room = req.body;
-    //   const result = roomsCollection.insertOne(room);
-    //   console.log(result);
-    //   res.send(result);
-    // });
+    // get role
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+    // get all rooms
+    app.get("/rooms", async (req, res) => {
+      const rooms = await roomsCollection.find().toArray();
+      res.send(rooms);
+    });
+    // get single room;
+    app.get("/room/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    });
     // Save a room in database
     app.post("/rooms", async (req, res) => {
       const room = req.body;
-      console.log(room);
+
       const result = await roomsCollection.insertOne(room);
       res.send(result);
     });
 
+    // update room booking status
+    app.patch("/rooms/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          booked: status,
+        },
+      };
+      const result = await roomsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    //
+    // get filter rooms for host
+    app.get("/rooms/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "hostData.email": email };
+      const result = await roomsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // delete room
+    app.delete("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // Get bookings for guest
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const query = { "guest.email": email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // Save a bookingInfo in database
+    app.post("/bookings", async (req, res) => {
+      const bookings = req.body;
+
+      const result = await bookingsCollection.insertOne(bookings);
+      res.send(result);
+    });
+
+    // delete a booking
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
